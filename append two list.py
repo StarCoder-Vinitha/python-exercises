7d0caecfb45b73ca4115ae769a53d45cf61170c3
StarCoder-Vinitha <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-15 01:28:12 +0530

    Add files via upload

---
+++ append two list.py
@@ -0,0 +1,9 @@
+ #Add two lists of elements and append the list of elements in new list
+my_lis = []
+def lis(l1,l2):
+    l = l1+l2
+    for i in range(0,len(l1)):
+        my_lis.append(l1[i]+l2[i])
+        l.append(my_lis)
+    print("my_lis :",my_lis)
+lis([1,2,3],[4,5,6])
