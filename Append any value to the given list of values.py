387585c61892ee6a189f35d8b6c7a385e9825868
StarCoder-Vinitha <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-15 15:38:06 +0530

    Add files via upload

---
+++ Append any value to the given list of values.py
@@ -0,0 +1,5 @@
+data = [1,2,3,4,5,6] # create a list
+print(data) #  printing statement of given list
+
+data.append(7) # add an element to the given list using append() method
+print(data) #  printing statement of given list after adding an integer
