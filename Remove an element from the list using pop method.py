6c49f1ed347093f57948288bcd6a340e11dbb533
StarCoder-Vinitha <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-15 15:41:08 +0530

    Add files via upload

---
+++ Remove an element from the list using pop method.py
@@ -0,0 +1,5 @@
+data = [1,2,3,4,5,6] # create a list
+print(data) #  printing statement of given list
+
+data.pop(2) # remove an element from the given list using pop() method
+print(data) #  printing statement of given list after adding an integer
