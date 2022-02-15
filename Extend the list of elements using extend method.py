753c0871f264333701b935da2148149faeeae6b9
StarCoder-Vinitha <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-15 16:03:20 +0530

    Add files via upload

---
+++ Extend the list of elements using extend method.py
@@ -0,0 +1,6 @@
+my_data = [1,2,3,4,5,6]  # create a list
+print(my_data) #  printing statement of given list
+
+
+my_data.extend([7,8,9]) # extending an integer to the list
+print(my_data) # printing statement after inserting the integer
