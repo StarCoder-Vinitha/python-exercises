ae892e581a551d452518639d6d6ab28fe3416b4a
Vinitha Bolle <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-24 23:36:12 +0530

    Insert values to the list.py

--- Insert values to the list.py
+++
@@ -1,9 +0,0 @@
-my_data = [1,2,3,4,5,6]  # create a list
-print(my_data) #  printing statement of given list
-
-
-my_data.insert(8,9) # inserting an integer to the list
-print(my_data) # printing statement after inserting the integer
-
-my_data.insert(0,7) # inserting an element to the list before the given list
-print(my_data) # printing statement after inserting the integer
