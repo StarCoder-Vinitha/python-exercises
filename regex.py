9c8a9ed14e55feaf57d9f95cfe40969869233800
Vinitha Bolle <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-03-16 23:37:34 +0530

    Add files via upload
    Regular Expressions

---
+++ regex.py
@@ -0,0 +1,37 @@
+import re 
+name = "Regular expressions are handled as strings by Python. However"
+#x = (re.search('e',name))
+#print(x)
+#print(len('e'))
+
+
+#x = (re.findall('r',name))
+#print(len(x))
+#print(x)
+ 
+x = (re.match('R',name))
+print(x)
+
+#x = (re.findall('r+',name))
+#print(x)
+
+#x = (re.findall('r*',name))     # using findall function we are finding pattern from the sample 
+#print(x)
+
+#x = (re.findall('^e',name))   # ^ matches from beging of the string 
+#print(x)
+
+#x = (re.findall('e?',name))  # zero or one repetation 
+#print(x)
+
+#x = (re.compile(["a-z"]))
+
+
+
+
+#x = (re.search('strings') , x)
+#print('start index:',match.start())
+#print('end index:',match.end())
+p = 'python'
+print(lambda p:string.upper(p))
+print(1//5 , true)
